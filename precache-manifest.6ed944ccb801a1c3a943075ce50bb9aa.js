9fffe47fab8c68dac9d59f5318c6e5422d26067d
Dave Collison <daverbcollison@gmail.com>
2020-06-11 10:48:17 +0100

    Updates

---
+++ precache-manifest.6ed944ccb801a1c3a943075ce50bb9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9074dcd5e813fd8a0547332356b3c3b",
+    "url": "/fictional-books-library/index.html"
+  },
+  {
+    "revision": "4d82e3c1f7b9a1380c67",
+    "url": "/fictional-books-library/static/css/main.0d9d71cf.chunk.css"
+  },
+  {
+    "revision": "7eb2f130652565002c92",
+    "url": "/fictional-books-library/static/js/2.3a9cb790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fictional-books-library/static/js/2.3a9cb790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d82e3c1f7b9a1380c67",
+    "url": "/fictional-books-library/static/js/main.af18132e.chunk.js"
+  },
+  {
+    "revision": "f475493fa44e44864a18",
+    "url": "/fictional-books-library/static/js/runtime-main.bcff7d62.js"
+  }
+]);
